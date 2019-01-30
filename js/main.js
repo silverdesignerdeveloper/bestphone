c1a1ff7c18f2e896193493ec34fed1c577c0766b
Unknown <silver.designer.developer@gmail.com>
2019-01-30 15:26:16 +0100

    Load Random Image Phone

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ function calcoloPercentuale(val, tot) {
 
 function ratingLeft() { 
     document.getElementById('numberLeft').value = ++phoneLeft;
-    var percLeft = calcoloPercentuale(phoneLeft, phoneLeft + phoneRight)
-    var percRight = calcoloPercentuale(phoneRight, phoneLeft + phoneRight)
+    var percLeft = calcoloPercentuale(phoneLeft, phoneLeft + phoneRight);
+    var percRight = calcoloPercentuale(phoneRight, phoneLeft + phoneRight);
     
     document.getElementById('percentualeLeft').value = percLeft;
     document.getElementById('percentualeRight').value = percRight;
@@ -16,11 +16,23 @@ function ratingLeft() {
 
 function ratingRight() {
   document.getElementById('numberRight').value = ++phoneRight;
-  var percLeft = calcoloPercentuale(phoneLeft, phoneLeft + phoneRight)
-  var percRight = calcoloPercentuale(phoneRight, phoneLeft + phoneRight)
+  var percLeft = calcoloPercentuale(phoneLeft, phoneLeft + phoneRight);
+  var percRight = calcoloPercentuale(phoneRight, phoneLeft + phoneRight);
 
   document.getElementById('percentualeLeft').value = percLeft;
   document.getElementById('percentualeRight').value = percRight;
 }
 
+
+var imagesArray = ["iphonex.png", "samsunggalaxys8.png"];
+
+function loadRandomPhone() {
+  alert("Load Script Random image phone");
+  var num = Math.floor(Math.random() * 2); // 0...6
+  document.canvas.src = 'images/'+ imagesArray[num];
+  
+
+  var num2 = Math.floor(Math.random() * 2); // 0...6
+  document.canvas2.src = 'images/'+ imagesArray[num2];
+}
 
